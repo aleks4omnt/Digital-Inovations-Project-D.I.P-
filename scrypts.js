891695c766a64ea139be6f7e38e9a46510617275
aleks4omnt <aleks4omnt@gmail.com>
2017-06-15 04:19:59 +0300

    Add files via upload

--- scrypts.js
+++ scrypts.js
@@ -1,3 +1,47 @@
+//drag&drop related functions
+
+		var DragVar="";
+function showCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+	y = y-25;
+	x = x-25;
+disp_prompt_Div(x,y);
+}
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("info", ev.target.id);
+	DragVar="info";
+}
+function drag2(ev) {
+    ev.dataTransfer.setData("info2", ev.target.id);
+	DragVar="info2";
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData(DragVar);
+var nodeCopy = document.getElementById(data).cloneNode(true);
+  nodeCopy.id = "newId";
+  ev.target.appendChild(nodeCopy);	  
+  switch(DragVar) {
+    case "info":
+    showCoords(ev);
+	DragVar="";
+        break;
+    case "info2":
+		toggle_visibility();
+		DragVar="";
+        break;
+		
+    default:
+       alert('Default case');
+	   } 
+  
+}//End of Drag&Drop
 		var n=1;
 
 function toggle_visibility() 
@@ -5,23 +49,24 @@ function toggle_visibility()
         var e = document.getElementById("Frame");
 		var c = document.getElementById("exclamation");
 		
-        if ( e.style.display == 'block' ){
-			e.style.display = 'none';
+        if ( e.style.visibility == 'visible' ){
+			e.style.visibility = 'hidden';
 			c.style.display = 'block';
 		}
         else{
-            e.style.display = 'block';
+            e.style.visibility = 'visible';
 			c.style.display = 'none';
 
 		}
 	}
-var area2;
-function toggleArea2() {
-	if(!area2) {
-		area2 = new nicEditor({fullPanel : true}).panelInstance('EditBox',{hasPanel : true});
+var area1;
+
+function toggleArea1() {
+	if(!area1) {
+		area1 = new nicEditor({fullPanel : true}).panelInstance('EditBox',{hasPanel : true});
 	} else {
-		area2.removeInstance('EditBox');
-		area2 = null;
+		area1.removeInstance('EditBox');
+		area1 = null;
 	}
 }
 
